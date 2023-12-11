08d12789eb914dcc04f2e2d104a153a6f4f2eb4d
Sagar Bhat <sagar.jobs@outlook.com>
2023-12-11 20:19:36 +1100

    Add sample data.sql file to insert sample test data to the DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- Sample data insertion for Person table
+INSERT INTO Person (first_name, last_name, birth_date) VALUES ('Ross', 'Smith', TO_DATE('1994-05-15', 'YYYY-MM-DD'));
+INSERT INTO Person (first_name, last_name, birth_date) VALUES ('Martin', 'Taylo', TO_DATE('1974-08-22', 'YYYY-MM-DD'));
+-- Add more sample data as needed
